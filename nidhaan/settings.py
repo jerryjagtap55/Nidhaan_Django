43f68e56afff88458f873fa0b30c3a785a994f47
Nishant Ajay Jagtap <jerry.jagtap5@gmail.com>
2020-07-30 19:22:52 +0530

    local settings

--- nidhaan/settings.py
+++ nidhaan/settings.py
@@ -144,3 +144,10 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'nidhandesignstudio@gmail.com'
 EMAIL_HOST_PASSWORD = 'edngcydfzskamdiw'
 EMAIL_USE_TLS = True
+
+
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
